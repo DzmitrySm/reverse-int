659b65ccafeaa30df31f6ae4ea966cccb3e09e83
DzmitrySm <magicjoker@yandex.ru>
2022-01-08 21:23:47 +0300

    complete task reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
+  let modulN = Math.abs(n)
+  let newType = modulN.toString();
+  let i = newType.length - 1;
+  let result = '' ;
+  while (i >= 0) {
+      result = result + newType[i] ;
+      i = i - 1;
+  }
   
+
+return Math.floor(result);
 }
+
